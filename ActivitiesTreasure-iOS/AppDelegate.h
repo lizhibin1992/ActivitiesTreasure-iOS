9d2fc40c04d87a76ac7e57bdc90e88f883baeab5
Terry <Terry@TerrydeMacBook-Air.local>
2014-08-27 10:57:49 +0800

    Initial Commit

---
+++ ActivitiesTreasure-iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ActivitiesTreasure-iOS
+//
+//  Created by Terry on 14-8-27.
+//  Copyright (c) 2014年 Terry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ActivitiesTreasure-iOSTests/ActivitiesTreasure_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  ActivitiesTreasure_iOSTests.m
+//  ActivitiesTreasure-iOSTests
+//
+//  Created by Terry on 14-8-27.
+//  Copyright (c) 2014年 Terry. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ActivitiesTreasure_iOSTests : XCTestCase
+
+@end
+
+@implementation ActivitiesTreasure_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
